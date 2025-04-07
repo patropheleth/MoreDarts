2be8652cb4924ebf22fadf2de86e793945b8c864
pat <patmeplease@mail.ru>
2025-04-08 02:37:16 +0800

    2.5 new darts, bobo granada

---
+++ Content/Items/Ammo/BoboGrenade.cs
@@ -0,0 +1,37 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace MoreDarts.Content.Items.Ammo { 
+	public class BoboGrenade : ModItem {
+		// The Display Name and Tooltip of this item can be edited in the 'Localization/en-US_Mods.MoreDarts.hjson' file.
+		public override void SetStaticDefaults() {
+			Item.ResearchUnlockCount = 99;
+		}
+		public override void SetDefaults() {
+			Item.width = 14;
+			Item.height = 36;
+
+			Item.damage = 60; 
+			Item.DamageType = DamageClass.Ranged;
+
+			Item.maxStack = Item.CommonMaxStack;
+			Item.consumable = true;
+			Item.useStyle = ItemUseStyleID.Swing;
+			Item.knockBack = 1.5f;
+			Item.useTime = 40;
+			Item.useAnimation = 40;
+			Item.value = Item.sellPrice(copper: 16);
+			Item.shoot = ModContent.ProjectileType<Projectiles.BoboGrenadeProjectile>(); // The projectile that weapons fire when using this item as ammunition.
+			Item.shootSpeed = 3f; // The speed of the projectile.
+		}
+
+		public override void AddRecipes()
+		{
+			Recipe recipe = CreateRecipe();
+			recipe.AddIngredient(ItemID.DirtBlock, 10);
+			recipe.AddTile(TileID.WorkBenches);
+			recipe.Register();
+		}
+	}
+}
---
+++ Content/Projectiles/SandDartProjectile.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Numerics;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace MoreDarts.Content.Projectiles {
+	public class SandDartProjectile : ModProjectile {
+		public override void SetStaticDefaults() {
+			// If this arrow would have strong effects (like Holy Arrow pierce), we can make it fire fewer projectiles from Daedalus Stormbow for game balance considerations like this:
+			//ProjectileID.Sets.FiresFewerFromDaedalusStormbow[Type] = true;			
+			ProjectileID.Sets.Explosive[Type] = true;
+		}
+
+		public override void SetDefaults() {
+			Projectile.width = 10; // The width of projectile hitbox
+			Projectile.height = 10; // The height of projectile hitbox
+
+			Projectile.friendly = true;
+			Projectile.DamageType = DamageClass.Ranged;
+			Projectile.timeLeft = 120;
+			Projectile.penetrate = -1;
+			Projectile.usesLocalNPCImmunity = true;
+			Projectile.localNPCHitCooldown = -1;
+		}
+
+		public override void AI() {
+			// The code below was adapted from the ProjAIStyleID.Arrow behavior. Rather than copy an existing aiStyle using Projectile.aiStyle and AIType,
+			// like some examples do, this example has custom AI code that is better suited for modifying directly.
+			// See https://github.com/tModLoader/tModLoader/wiki/Basic-Projectile#what-is-ai for more information on custom projectile AI.
+
+			// Apply gravity after a quarter of a second
+			if (Projectile.owner == Main.myPlayer && Projectile.timeLeft <= 3) {
+				Projectile.PrepareBombToBlow();
+			}
+			Projectile.ai[0] += 1f;
+			if (Projectile.ai[0] >= 15f) {
+				Projectile.ai[0] = 15f;
+				Projectile.velocity.Y += 0.1f;
+			}
+			if (Main.myPlayer == Projectile.owner) {
+
+			}
+			// The projectile is rotated to face the direction of travel
+			Projectile.rotation = Projectile.velocity.ToRotation() + MathHelper.PiOver2;
+
+			// Cap downward velocity
+			if (Projectile.velocity.Y > 16f) {
+				Projectile.velocity.Y = 16f;
+			}
+		}
+		public override bool OnTileCollide(Microsoft.Xna.Framework.Vector2 oldVelocity) {
+			Projectile.timeLeft = 3;
+			return false;
+		}
+
+		public override void PrepareBombToBlow() {
+			Projectile.tileCollide = false; // This is important or the explosion will be in the wrong place if the grenade explodes on slopes.
+			Projectile.alpha = 255;
+			Projectile.Resize(256, 256);
+			Projectile.damage /= 5;
+		} 
+		public override void OnKill(int timeLeft) {
+			SoundEngine.PlaySound(SoundID.Dig, Projectile.position); // Plays the basic sound most projectiles make when hitting blocks.
+			
+			Projectile.Resize(256, 256);
+			for (int i = 0; i < 5; i++) // Creates a splash of dust around the position the projectile dies.
+			{
+				Dust dust = Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, DustID.Silver);
+				dust.noGravity = true;
+				dust.velocity *= 1.5f;
+				dust.scale *= 0.9f;
+			} 
+			for (int j = 0; j < 20; j++) {
+				var fireDust = Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, DustID.Torch, 0f, 0f, 100, default, 3.5f);
+				fireDust.noGravity = true;
+				fireDust.velocity *= 7f;
+				fireDust = Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, DustID.Torch, 0f, 0f, 100, default, 1.5f);
+				fireDust.velocity *= 3f;
+			}
+		}
+	}
+}
